523a59132014bc327b16cf217653ddba24c25251
Emilio J. Franco <ejfranco06@gmail.com>
2018-08-09 18:08:34 -0400

    Refactor to use a cat class

--- js/app.js
+++ js/app.js
@@ -1,7 +1,62 @@
-const catButton = document.getElementsByClassName('cat-pic')[0];
-let clickCounter = 0;
-catButton.addEventListener('click', () => {
-  const clicksEl = document.getElementsByClassName('clicks')[0];
-  clickCounter++;
-  clicksEl.innerHTML = `Clicks: ${clickCounter}`;
-});
+class Cat {
+  constructor (name, image) {
+    this.name = name;
+    this.image = image;
+    this.clicks = 0;
+  }
+
+  increaseClicks () {
+    this.clicks++;
+  }
+
+  addToList () {
+    const catListEl = document.getElementsByClassName('cat-list')[0];
+    const fragment = document.createDocumentFragment();
+
+    const catContainerEl = document.createElement('div');
+    catContainerEl.setAttribute('class', 'cat-container');
+
+    const infoPanelEl = document.createElement('div');
+    infoPanelEl.setAttribute('class', 'info-panel');
+    catContainerEl.appendChild(infoPanelEl);
+
+    const nameEl = document.createElement('span');
+    nameEl.setAttribute('class', 'name');
+    nameEl.innerHTML = `Name: ${this.name}`;
+    infoPanelEl.appendChild(nameEl);
+
+    const clicksEl = document.createElement('span');
+    clicksEl.setAttribute('class', 'clicks');
+    clicksEl.innerHTML = `Clicks: ${this.clicks}`;
+    infoPanelEl.appendChild(clicksEl);
+
+    const figureEl = document.createElement('figure');
+    const imgEl = document.createElement('img');
+    imgEl.setAttribute('class', 'cat-pic');
+    imgEl.setAttribute('src', `${this.image}`);
+    imgEl.setAttribute('alt', 'cat');
+    figureEl.appendChild(imgEl);
+    catContainerEl.appendChild(figureEl);
+
+    fragment.appendChild(catContainerEl);
+
+    catListEl.appendChild(fragment);
+
+    this.addClick(imgEl, clicksEl);
+  }
+
+  addClick (target, output) {
+    target.addEventListener('click', () => {
+      this.increaseClicks();
+      output.innerHTML = `Clicks: ${this.clicks}`;
+    });
+  }
+}
+
+const catSet = [
+  new Cat('xuxa', 'image/xuxa.jpg'),
+  new Cat('chewie', 'image/chewie.jpg')
+];
+
+catSet[0].addToList();
+catSet[1].addToList();
