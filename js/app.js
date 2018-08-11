1bc4d74926a1077aff8355649b1ded0a3e2a0bab
Emilio J. Franco <ejfranco06@gmail.com>
2018-08-11 11:15:00 -0400

    Refactor to cat clicker premium

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// ================Model===========================
 class Cat {
   constructor (name, image) {
     this.name = name;
@@ -7,49 +8,6 @@ class Cat {
 
   increaseClicks () {
     this.clicks++;
-  }
-
-  addToList () {
-    const catListEl = document.getElementsByClassName('cat-model')[0];
-    const fragment = document.createDocumentFragment();
-
-    const catContainerEl = document.createElement('div');
-    catContainerEl.setAttribute('class', 'cat-container');
-
-    const infoPanelEl = document.createElement('div');
-    infoPanelEl.setAttribute('class', 'info-panel');
-    catContainerEl.appendChild(infoPanelEl);
-
-    const nameEl = document.createElement('span');
-    nameEl.setAttribute('class', 'name');
-    nameEl.innerHTML = `Name: ${this.name}`;
-    infoPanelEl.appendChild(nameEl);
-
-    const clicksEl = document.createElement('span');
-    clicksEl.setAttribute('class', 'clicks');
-    clicksEl.innerHTML = `Clicks: ${this.clicks}`;
-    infoPanelEl.appendChild(clicksEl);
-
-    const figureEl = document.createElement('figure');
-    const imgEl = document.createElement('img');
-    imgEl.setAttribute('class', 'cat-pic');
-    imgEl.setAttribute('src', `${this.image}`);
-    imgEl.setAttribute('alt', 'cat');
-    figureEl.appendChild(imgEl);
-    catContainerEl.appendChild(figureEl);
-
-    fragment.appendChild(catContainerEl);
-
-    catListEl.appendChild(fragment);
-
-    this.addClick(imgEl, clicksEl);
-  }
-
-  addClick (target, output) {
-    target.addEventListener('click', () => {
-      this.increaseClicks();
-      output.innerHTML = `Clicks: ${this.clicks}`;
-    });
   }
 }
 
@@ -65,10 +23,62 @@ const catSet = [
   {
     name: 'jetske',
     image: 'image/jetske.jpg'
+  },
+  {
+    name: 'dave',
+    image: 'image/dave.jpg'
   }
 ];
+const allCats = [];
 
-function createList () {
+// ================Model===========================
+
+// ===============View=============================
+
+function renderCat (cat) {
+  const catModelEl = document.getElementsByClassName('cat-model')[0];
+  const fragment = document.createDocumentFragment();
+
+  const catContainerEl = document.createElement('div');
+  catContainerEl.setAttribute('class', 'cat-container');
+
+  const infoPanelEl = document.createElement('div');
+  infoPanelEl.setAttribute('class', 'info-panel');
+  catContainerEl.appendChild(infoPanelEl);
+
+  const nameEl = document.createElement('span');
+  nameEl.setAttribute('class', 'name');
+  nameEl.innerHTML = `Name: ${cat.name}`;
+  infoPanelEl.appendChild(nameEl);
+
+  const clicksEl = document.createElement('span');
+  clicksEl.setAttribute('class', 'clicks');
+  clicksEl.innerHTML = `Clicks: ${cat.clicks}`;
+  infoPanelEl.appendChild(clicksEl);
+
+  const figureEl = document.createElement('figure');
+  const imgEl = document.createElement('img');
+  imgEl.setAttribute('class', 'cat-pic');
+  imgEl.setAttribute('src', `${cat.image}`);
+  imgEl.setAttribute('alt', 'cat');
+  figureEl.appendChild(imgEl);
+  catContainerEl.appendChild(figureEl);
+
+  fragment.appendChild(catContainerEl);
+  catModelEl.innerHTML = '';
+  catModelEl.appendChild(fragment);
+
+  handleClick(imgEl, clicksEl, cat);
+}
+
+function handleClick (target, output, cat) {
+  target.addEventListener('click', () => {
+    cat.increaseClicks();
+    output.innerHTML = `Clicks: ${cat.clicks}`;
+  });
+}
+
+function renderCatList (catSet) {
   const catSelectorEl = document.getElementsByClassName('cat-selecter')[0];
   const fragment = document.createDocumentFragment();
 
@@ -82,21 +92,29 @@ function createList () {
   });
 
   catSelectorEl.appendChild(fragment);
+}
+// ===============View=============================
 
+// ================Octupus========================
+function addHandleSelect () {
+  const catSelectorEl = document.getElementsByClassName('cat-selecter')[0];
   catSelectorEl.addEventListener('change', (event) => {
     const catModelEl = document.getElementsByClassName('cat-model')[0];
     const catIndex = event.target.value;
     console.log(catIndex);
     catModelEl.innerHTML = '';
-    allCats[catIndex].addToList();
+    renderCat(allCats[catIndex]);
   });
 }
 
-const allCats = [];
-catSet.forEach((cat) => {
-  allCats.push(new Cat(cat.name, cat.image));
-});
+function init () {
+  renderCatList(catSet);
+  addHandleSelect();
+  catSet.forEach((cat) => {
+    allCats.push(new Cat(cat.name, cat.image));
+  });
+  renderCat(allCats[0]);
+}
+// ================Octupus========================
 
-allCats[0].addToList();
-
-createList();
+init();
